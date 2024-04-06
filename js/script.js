3a644c4a9cdb21f5d6bbba3b8425954dbc17ab3c
Kantaro Nakaya <shimeno38saiko@gmail.com>
2024-04-06 18:03:30 +0900

    アニメーションのjsをwow.jsに変更

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 //=====画面読込時のjquery
 $(window).on('load',function(){
 $("#splash-logo").delay(0).fadeOut('slow');
@@ -12,36 +11,3 @@ $('body').addClass('appear');//フェードアウト後bodyにappearクラス付
 //=====ここまでローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
 
 });
-
-//====================================
-//====================================
-//====================================
-//====================================
-//====================================
-
-
-// 動きのきっかけとなるアニメーションの名前を定義
-function fadeAnime(){
-
-//ふわっと動くきっかけのクラス名と動きのクラス名の設定
-$('.fadeUpTrigger').each(function(){ //fadeInUpTriggerというクラス名が
-　　var elemPos = $(this).offset().top-50; //要素より、50px上の
-　　var scroll = $(window).scrollTop();
-　　var windowHeight = $(window).height();
-　　if (scroll >= elemPos - windowHeight){
-　　$(this).addClass('fadeUp');
-　　// 画面内に入ったらfadeInというクラス名を追記
-　　}else{
-　　　$(this).removeClass('fadeUp');
-　　// 画面外に出たらfadeInというクラス名を外す
-　　}
-　　});
-
-
-}//ここまでふわっと動くきっかけのクラス名と動きのクラス名の設定
-
-// 画面をスクロールをしたら動かしたい場合の記述
-  $(window).scroll(function (){
-    fadeAnime();/* アニメーション用の関数を呼ぶ*/
-  });// ここまで画面をスクロールをしたら動かしたい場合の記述
-
