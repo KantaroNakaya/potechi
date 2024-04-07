29948ced36645c72a9539e91a17e028eff9d9751
Kantaro Nakaya <shimeno38saiko@gmail.com>
2024-04-07 12:05:37 +0900

    ローディングの発動を初回読み込み時に限定

--- js/load.js
+++ js/load.js
@@ -1,13 +1,19 @@
 //=====画面読込時のjquery
 $(window).on('load',function(){
-$("#splash-logo").delay(0).fadeOut('slow');
+  if (!sessionStorage.getItem('isVisited')) {
+    $("#splash-logo").delay(0).fadeOut('slow');
 
-//=====ここからローディングエリア（splashエリア）をフェードアウトした後に動かしたいJSをまとめる
-$("#splash").delay(1000).fadeOut('slow',function(){
+    //=====ここからローディングエリア（splashエリア）をフェードアウトした後に動かしたいJSをまとめる
+    $("#splash").delay(1000).fadeOut('slow',function(){
 
-$('body').addClass('appear');//フェードアウト後bodyにappearクラス付与
+    $('body').addClass('appear');//フェードアウト後bodyにappearクラス付与
 
-});
-//=====ここまでローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
-
+    });
+    //=====ここまでローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
+    sessionStorage.setItem('isVisited', 'true');
+  } else {
+    $("#splash-logo").hide();
+    $("#splash").hide();
+    $('body').addClass('appear');
+  }
 });
